4ab58b7ec80ad0e848d498e20363a4a57157f3aa
Ruben Alvarado <ruben.alvardo@plexusworldwide.com>
2025-05-03 19:28:34 -0600

    feat(seeding): add seed files for database initialization
    This commit introduces seed files for various database tables, including languages, question sets, EHR providers, clients, patients, audit logs, bulk operations, EHR sync logs, questions, EHR mappings cache, base questions, and EHR mappings. These seed files are used to populate the database with initial data for development and testing purposes.

--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -1,9 +1,39 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { PrismaClient } from '@prisma/client';
+import { seedLanguages } from './seeds/languages.seed';
+import { seedEhrProviders } from './seeds/ehrProviders.seed';
+import { seedClients } from './seeds/clients.seed';
+import { seedBaseQuestions } from './seeds/baseQuestions.seed';
+import { seedEhrMappings } from './seeds/ehrMappings.seed';
+import { seedQuestionSets } from './seeds/questionSets.seed';
+import { seedPatients } from './seeds/patients.seed';
+import { seedPatientResponses } from './seeds/patientResponses.seed';
+import { seedEhrSyncLogs } from './seeds/ehrSyncLogs.seed';
+import { seedBulkOperations } from './seeds/bulkOperations.seed';
+import { seedAuditLogs } from './seeds/auditLogs.seed';
+import { seedEhrMappingsCache } from './seeds/ehrMappingsCache.seed';
+import { seedQuestions } from './seeds/questions.seed';
 
 const prisma = new PrismaClient();
 
-async function main() {}
+async function main() {
+  console.log('Start seeding...');
+  await seedLanguages(prisma);
+  await seedEhrProviders(prisma);
+  const clientsIds = await seedClients(prisma);
+  await seedBaseQuestions(prisma);
+  await seedEhrMappings(prisma);
+  await seedQuestionSets(prisma, clientsIds);
+  const patientIds = await seedPatients(prisma, clientsIds);
+  await seedPatientResponses(prisma, patientIds);
+  await seedEhrSyncLogs(prisma, patientIds);
+  await seedBulkOperations(prisma, clientsIds);
+  await seedAuditLogs(prisma);
+  await seedEhrMappingsCache(prisma, clientsIds);
+  await seedQuestions(prisma);
+
+  console.log('Seed completed successfully!');
+}
 
 main()
   .catch((e) => {
